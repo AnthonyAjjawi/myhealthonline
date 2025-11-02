80cdc477d049bd8474b5ac498d697df78fabf35c
anthony ajjawi <ajjawi_amer@hotmail.com>
2025-11-01 22:16:52 -0400

    worked the third and fourth section

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
-  const menuItems = document.querySelector(".nav-list");
+  const menuItems = document.querySelector(".mobile-menu");
   const closeBtn = document.querySelector(".close-btn");
   const menuIcon = document.querySelector(".mobile-logo");
 
   // Open menu
   menuIcon.addEventListener("click", () => {
     menuItems.classList.toggle("active");
+    document.body.classList.toggle("no-sroll");
   });
 
   // Close menu
   closeBtn.addEventListener("click", () => {
     menuItems.classList.remove("active");
+    document.body.classList.toggle("no-sroll");
   });
 });
